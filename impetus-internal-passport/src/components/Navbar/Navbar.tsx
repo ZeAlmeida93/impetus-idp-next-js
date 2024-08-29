d12098a147a6651ee7a8dd7dd5b39b9d20fdbcc4
Jose Almeida <gralhastudio@gmail.com>
2024-08-29 21:58:20 +0100

    navbar update

--- impetus-internal-passport/src/components/Navbar/Navbar.tsx
+++ impetus-internal-passport/src/components/Navbar/Navbar.tsx
@@ -1,28 +1,35 @@
 import React, { useState } from 'react';
-import styles from './Navbar.module.scss'; // Importing the CSS module
+import styles from './Navbar.module.scss';
+import logo from '../Logo/logo-teste.svg'; // Import the SVG file
 
 const Navbar: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [isActive, setIsActive] = useState(false); // State to manage active class for bars
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
+        setIsActive(!isActive); // Toggle the active state for bars
     };
 
     return (
         <nav className={styles.navbar}>
             <div className={styles.logo}>
-                <a href="/">MyLogo</a>
+                <a href="/">
+                    <img width="100%" src={logo} alt="Logo" />
+                </a>
             </div>
             <ul className={`${styles.links} ${isOpen ? styles.open : ''}`}>
                 <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="#about">Suppliers</a></li>
+                <li><a href="#about">Production Orders</a></li>
+                <li><a href="#services">Certifications</a></li>
+                <li><a href="#contact">Products</a></li>
+                <li><a href="#contact">Monitors</a></li>
             </ul>
             <div className={styles.toggle} onClick={toggleMenu}>
-                <span className={styles.bar}></span>
-                <span className={styles.bar}></span>
-                <span className={styles.bar}></span>
+                <span className={`${styles.bar} ${isActive ? styles.active : ''}`}></span>
+                <span className={`${styles.bar} ${isActive ? styles.active : ''}`}></span>
+                <span className={`${styles.bar} ${isActive ? styles.active : ''}`}></span>
             </div>
         </nav>
     );
